5deec7f04e966f77adab83a69cd061b2afed697d
Rizame <53150959+Rizame@users.noreply.github.com>
2025-10-05 21:29:44 +0300

    0058-LENGTH-OF-LAST-WORD Stats: Time: 0 ms (100%), Space: 8 MB (99.97%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int index = s.length() - 1;
+        int count = 0;
+
+        while (index >= 0 && s[index] == ' ') {
+            index--;
+        }
+
+        while (index >= 0 && s[index] != ' ') {
+            count++;
+            index--;
+        }
+
+        return count;
+    }
+};

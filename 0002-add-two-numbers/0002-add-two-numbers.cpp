8c5e030b60fba925a6d608ce2c895705fd991b9f
Rizame <53150959+Rizame@users.noreply.github.com>
2025-10-06 17:51:31 +0300

    0002-ADD-TWO-NUMBERS Stats: Time: 0 ms (100%), Space: 76.9 MB (92.34%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,34 +1,18 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* res = new ListNode();
-        helper(l1, l2, res, 0);
-        return res;
+        return helper(l1, l2, 0);
     }
-    void helper(const ListNode* l1, const ListNode* l2, ListNode* res, int carry){
-        
-        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
-        res->val = sum % 10;
-        int newCarry = sum / 10;
 
-        if ((l1 && l1->next) || (l2 && l2->next) || newCarry > 0) {
-            res->next = new ListNode();
-            helper(l1 ? l1->next : nullptr, 
-                l2 ? l2->next : nullptr, 
-                res->next, 
-                newCarry);
-        } else {
-            res->next = nullptr;  
-        }
+    ListNode* helper(const ListNode* l1, const ListNode* l2, int carry) {
+        if (!l1 && !l2 && carry == 0)
+            return nullptr; 
+
+        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+        ListNode* node = new ListNode(sum % 10);
+        node->next = helper(l1 ? l1->next : nullptr,
+                            l2 ? l2->next : nullptr,
+                            sum / 10);
+        return node;
     }
 };

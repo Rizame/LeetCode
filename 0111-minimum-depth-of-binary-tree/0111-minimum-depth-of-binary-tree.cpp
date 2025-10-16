dd9e3d4a0f7528007c02d08e2d4dd6ed27bbbecc
Rizame <53150959+Rizame@users.noreply.github.com>
2025-10-16 22:53:48 +0300

    0111-MINIMUM-DEPTH-OF-BINARY-TREE Stats: Time: 0 ms (100%), Space: 146.9 MB (22.19%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -12,13 +12,9 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-     if (!root) return 0;
-        if(root->left && root->right){
-            return 1 + min(minDepth(root->left), minDepth(root->right));
-        }
-        else if(!root->left && !root->right){
-            return 1;
-        }
-        else  return 1 + max(minDepth(root->left), minDepth(root->right));
-    }
+    if (!root) return 0;
+    if (!root->left) return 1 + minDepth(root->right);
+    if (!root->right) return 1 + minDepth(root->left);
+    return 1 + min(minDepth(root->left), minDepth(root->right));
+}
 };

8840fbf3d71e0baffbb5b1c6c285f6ddc43ff85f
Rizame <53150959+Rizame@users.noreply.github.com>
2025-10-19 00:04:24 +0300

    0119-PASCALS-TRIANGLE-II Stats: Time: 0 ms (100%), Space: 9 MB (52.59%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-       vector<vector<int>> res;
-        res.push_back({1});
-        if(rowIndex == 0) return res[0];
+        vector<int> res(rowIndex+1,1);
+        vector<int> temp(rowIndex+1,1);
+        if(rowIndex == 0) return res;
         for(int i = 1; i <= rowIndex;i++){
-            res.push_back(vector<int>(i + 1, 1));
-            for(int k = 0; k <i+1;k++){
-                if(k == 0 || k == i) res[i][k] = 1;
-                else{
-                    res[i][k] = res[i-1][k-1] + res[i-1][k];
-                }
+            for(int k = 1; k < i;k++){
+                res[k] = temp[k-1] + temp[k];
             }
+            temp = res;
         }
-        return res[rowIndex];
+        return res;
     }
 };
